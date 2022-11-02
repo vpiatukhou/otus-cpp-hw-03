e2af43e5979819ef7423b6452de006d6c8dcbaed
Vasilij <v.piatukhou@gmail.com>
2022-11-02 09:13:48 +0300

    Adjusted examples in main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -29,23 +29,23 @@ namespace Homework {
 }
 
 int main() {
-    const int initPoolSize = 10;
+    const std::size_t numberOfElements = 10;
 
     std::cout << "A map with a default allocator.\n" << std::endl;
     {
         std::map<int, int> map;
-        for (int i = 0; i < initPoolSize; ++i) {
+        for (int i = 0; i < numberOfElements; ++i) {
             map[i] = Homework::factorial(i);
         }
         Homework::printMap(map);
     }
     std::cout << "\nA map with a custom allocator (with a fixed number of elements).\n" << std::endl;
     {
-        using Allocator = Homework::MyAllocator<std::pair<const int, int>, initPoolSize, Homework::FixedPoolStrategy>;
+        using Allocator = Homework::MyAllocator<std::pair<const int, int>, numberOfElements, Homework::FixedPoolStrategy>;
 
         std::map<int, int, std::less<int>, Allocator> map;
         
-        for (int i = 0; i < initPoolSize; ++i) {
+        for (int i = 0; i < numberOfElements; ++i) {
             map[i] = Homework::factorial(i);
         }
 
@@ -53,22 +53,22 @@ int main() {
     }
     std::cout << "\nA map with a custom allocator (with a variable number of elements).\n" << std::endl;
     {
-        std::size_t numberOfElements = initPoolSize * 3; //a total number of elements is bigger than the initial pool size => the pool will grow
+        const std::size_t initPoolSize = 4; //the pool size is less than numberOfElements
         using Allocator = Homework::MyAllocator<std::pair<const int, int>, initPoolSize, Homework::DynamicPoolStrategy>;
         
         std::map<int, int, std::less<int>, Allocator> map;
         
         for (int i = 0; i < numberOfElements; ++i) {
-            map[i] = i * 2; //factorial was replaced with i*2 because size of int (and even "long long") is not enough
+            map[i] = Homework::factorial(i);
         }
 
         Homework::printMap(map);
     }
     std::cout << "\nA custom linked list with a custom allocator (with a fixed number of elements).\n" << std::endl;
     {
-        using Allocator = Homework::MyAllocator<int, initPoolSize, Homework::FixedPoolStrategy>;
+        using Allocator = Homework::MyAllocator<int, numberOfElements, Homework::FixedPoolStrategy>;
         Homework::LinkedList<int, Allocator> list;
-        for (int i = 0; i < initPoolSize; ++i) {
+        for (int i = 0; i < numberOfElements; ++i) {
             list.add(Homework::factorial(i));
         }
 
@@ -80,11 +80,11 @@ int main() {
 
     std::cout << "\nA custom linked list with a custom allocator (with a variable number of elements).\n" << std::endl;
     {
-        std::size_t numberOfElements = initPoolSize * 3; //a total number of elements is bigger than the initial pool size => the pool will grow
+        const std::size_t initPoolSize = 4; //the pool size is less than numberOfElements
         using Allocator = Homework::MyAllocator<int, initPoolSize, Homework::DynamicPoolStrategy>;
         Homework::LinkedList<int, Allocator> list;
         for (int i = 0; i < numberOfElements; ++i) {
-            list.add(i * 2); //factorial was replaced with i*2 because size of int (and even "long long") is not enough
+            list.add(Homework::factorial(i));
         }
 
         //print all values
